b0daa704e94af637d1db4c0e423a9fdd2972194d
ltamon <lizheltamon@gmail.com>
2023-08-08 17:29:11 +0100

    Add template for SLURM job

---
+++ template_job.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+##########################################################################
+##########################################################################
+#SBATCH --time=0-24:00:00
+#SBATCH -p long
+#SBATCH --mem=2G
+#SBATCH -n 1
+#SBATCH --mail-user=ltamon
+#SBATCH --mail-type=ALL
+#########################################################################
+## JOB DETAILS * JOB DETAILS * JOB DETAILS * JOB DETAILS * JOB DETAILS ##
+#########################################################################
+#module add java/17.0.1
+#module add fastqc/0.11.9
+
+#/project/sahakyanlab/ltamon
+
+#fastq_dir="./../FASTQ"
+#files=(${fastq_dir}/*fastq.gz)
+
+for i in *fastq.gz
+do
+	fastqc $i -o "../FASTQC" --noextract -d "../FASTQC/temp"
+done
+
