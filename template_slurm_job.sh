ae0bb1421b96f324af3ed988ecc6b1f837cd3db2
ltamon <lizheltamon@gmail.com>
2023-09-02 00:24:39 +0100

    Add template for submitting slurm job

---
+++ template_slurm_job.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+##########################################################################
+## A script template for submitting batch jobs. To submit a batch job, 
+## please type
+##
+##    sbatch myprog.sh
+##
+## Please note that anything after the characters "#SBATCH" on a line
+## will be treated as a Slurm option.
+##########################################################################
+
+## Specify a partition. Check available partitions using sinfo Slurm command.
+#SBATCH --partition=short
+
+## The following line will send an email notification to your registered email
+## address when the job ends or fails.
+#SBATCH --mail-type=END,FAIL
+
+## Specify the amount of memory that your job needs. This is for the whole job.
+## Asking for much more memory than needed will mean that it takes longer to
+## start when the cluster is busy.
+#SBATCH --mem=10G
+
+## Specify the number of CPU cores that your job can use. This is only relevant for
+## jobs which are able to take advantage of additional CPU cores. Asking for more
+## cores than your job can use will mean that it takes longer to start when the
+## cluster is busy.
+#SBATCH --ntasks=1
+
+## Specify the maximum amount of time that your job will need to run. Asking for
+## the correct amount of time can help to get your job to start quicker. Time is
+## specified as DAYS-HOURS:MINUTES:SECONDS. This example is one day.
+#SBATCH --time=0-24:00:00
+
+## Capture the terminal output and save any error messages. This is very useful
+## if you have problems and need to ask for help.
+#SBATCH --output=%j_%x.out
+#SBATCH --error=%j_%x.err
+
+## Change directory to working directory
+mkdir -p /tmp/$USER
+cd /tmp/$USER
+
+## Load modules as required
+module load python-base/3.11.3
+module load R-base/4.3.0
+
+## Execute main script below
+output_file='slurm_template.out'
+echo USER=$USER > $output_file
+echo PATH=$PATH >> $output_file
+module list >> $output_file
